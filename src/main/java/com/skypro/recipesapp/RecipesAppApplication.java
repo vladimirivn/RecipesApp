4eb7fb234b52e177509d216ffd9e083f082855e5
SIDORYCH <vladimiriv@hotmail.com>
2023-02-06 21:13:26 +0300

    Введение в Maven. Первый коммит

---
+++ src/main/java/com/skypro/recipesapp/RecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.recipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesAppApplication.class, args);
+    }
+
+}
